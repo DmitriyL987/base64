5612ce2df025384601cab9b853c2ecb8212c8317
Dmitriy <89738132+DmitriyL987@users.noreply.github.com>
2022-05-23 20:39:56 +0300

    Create base64.h

---
+++ base64.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include <memory>
+#include <stdlib.h>
+#include <string>
+#include <cstring>
+
+std::string encode(const char* text);
+std::string decode(std::string& text);
